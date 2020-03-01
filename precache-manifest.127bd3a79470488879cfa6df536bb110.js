03340e42b15396a706eb537d5fadc525e2032cce
Tien Dinh <tiendinhphuc@gmail.com>
2020-02-29 20:50:56 -0600

    Updates

---
+++ precache-manifest.127bd3a79470488879cfa6df536bb110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f3b7aa46fc520c1ee7f9feb6064ca6",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "8df51c463479a0934c12",
+    "url": "/burger/static/css/main.e64a2d2e.chunk.css"
+  },
+  {
+    "revision": "1e2cbddb98854d83aabd",
+    "url": "/burger/static/js/2.650669d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/burger/static/js/2.650669d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df51c463479a0934c12",
+    "url": "/burger/static/js/main.a0fb458c.chunk.js"
+  },
+  {
+    "revision": "ccd9bcf50708292440df",
+    "url": "/burger/static/js/runtime-main.90a13846.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/logo.b8503d26.png"
+  }
+]);
